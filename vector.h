d4baf3b5ecb320286caad8fca0438d8883a4a0c8
bgamble01 <73133168+bgamble01@users.noreply.github.com>
2022-05-02 23:28:20 -0700

    Update vector.h

--- vector.h
+++ vector.h
@@ -1,2 +1,12 @@
 
 // TODO: function prototypes of vector and matrix operations
+int NPROC;
+int NRES;
+
+int * vcopyres(int *);
+int * vcopyproc(int *);
+int vcomp(int*, int*);
+void vadd(int*, int*);
+void vsub(int*, int*);
+void printM(int**);
+void printV(int*);
